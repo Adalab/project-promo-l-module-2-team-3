976d01a1fe9a6bf79c245593862a71209e32bc56
Marta Cano <martanchor@gmail.com>
2021-01-25 17:51:55 +0100

    name js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('>> Ready :)');
+const fullName = document.querySelector('.jsname');
+const previewName = document.querySelector('.jsnamepreview');
+
+function handleName() {
+    let nameValue = fullName.value;
+
+    if (nameValue === "") {
+        previewName.innerHTML = 'Nombre Apellidos';
+    } else {
+        previewName.innerHTML = nameValue;
+    }
+}
+
+
+fullName.addEventListener('keyup', handleName);
