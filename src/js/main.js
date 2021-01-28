6338b28fb984fc1bea774d099e18e100867fbc91
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-01-28 16:20:26 +0100

    Update main-index branch

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,72 @@
-'use strict';
+"use strict";
 //Name
-const fullName = document.querySelector('.jsname');
-const previewName = document.querySelector('.jsnamepreview');
+const fullName = document.querySelector(".jsname");
+const previewName = document.querySelector(".jsnamepreview");
 
 function handleName() {
-    let nameValue = fullName.value;
+  let nameValue = fullName.value;
 
-    if (nameValue === "") {
-        previewName.innerHTML = 'Paquita Salas';
-    } else {
-        previewName.innerHTML = nameValue;
-    }
+  if (nameValue === "") {
+    previewName.innerHTML = "Paquita Salas";
+  } else {
+    previewName.innerHTML = nameValue;
+  }
 }
 
-fullName.addEventListener('keyup', handleName);
+fullName.addEventListener("keyup", handleName);
 
 //Job
 
-const jobElement = document.querySelector('.jsjob');
-const previewJob = document.querySelector('.jsjobpreview');
+const jobElement = document.querySelector(".jsjob");
+const previewJob = document.querySelector(".jsjobpreview");
 
 function handleJob() {
-    let jobValue = jobElement.value;
+  let jobValue = jobElement.value;
 
-    if (jobValue === "") {
-        previewJob.innerHTML = 'CEO PS Management';
-    } else {
-        previewJob.innerHTML = jobValue;
-    }
+  if (jobValue === "") {
+    previewJob.innerHTML = "CEO PS Management";
+  } else {
+    previewJob.innerHTML = jobValue;
+  }
 }
 
-jobElement.addEventListener('keyup', handleJob);
-
-
+jobElement.addEventListener("keyup", handleJob);
 
 //Mail
-const mail = document.querySelector('.jsmail');
-const previewMail = document.querySelector('.jsmaillink');
+const mail = document.querySelector(".jsmail");
+const previewMail = document.querySelector(".jsmaillink");
 
 function handleMail() {
-    let mailValue = mail.value;
-    if (mailValue === '') {
-        previewMail.href = '#';
-    } else {
-
-        previewMail.href = `mailto:${mailValue}`;
-    }
+  let mailValue = mail.value;
+  if (mailValue === "") {
+    previewMail.href = "#";
+  } else {
+    previewMail.href = `mailto:${mailValue}`;
+  }
 }
-mail.addEventListener('keyup', handleMail);
+mail.addEventListener("keyup", handleMail);
 
 //Para que no haga scroll al pulsar el icono cuando no hay mail puesto
 
 function preventLink(ev) {
-    console.log(ev);
-    if (mail.value === '') {
-        ev.preventDefault()
-    }
+  console.log(ev);
+  if (mail.value === "") {
+    ev.preventDefault();
+  }
 }
-previewMail.addEventListener('click', preventLink)
+previewMail.addEventListener("click", preventLink);
 
 //Phone
-const telephone = document.querySelector('.jsphone');
-const previewTelephone = document.querySelector('.jsphonelink');
+const telephone = document.querySelector(".jsphone");
+const previewTelephone = document.querySelector(".jsphonelink");
 
 function handlePhone() {
-    let telephoneValue = telephone.value;
+  let telephoneValue = telephone.value;
 
-    if (telephoneValue === "") {
-        previewTelephone.href = '#';
-    } else {
-        previewTelephone.href = `tel:${telephoneValue}`;
-    }
+  if (telephoneValue === "") {
+    previewTelephone.href = "#";
+  } else {
+    previewTelephone.href = `tel:${telephoneValue}`;
+  }
 }
-telephone.addEventListener('keyup', handlePhone);
+telephone.addEventListener("keyup", handlePhone);
