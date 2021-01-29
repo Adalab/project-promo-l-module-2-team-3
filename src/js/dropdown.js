af6e431995f8c25705e5b8983d919694fa397d4f
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-01-29 12:30:22 +0100

    Add function open-close in JS

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
-const dropdownDesign = document.querySelector('.design');
-const dropdownFill = document.querySelector('.fill');
-const dropdownBtn = document.querySelector('.dropdown-btn');
-const dropdownFillBtn = document.querySelector('.dropdownfill-btn');
-const dropdownContainer = document.querySelector('.dropdowncontainer');
-const dropdownFillContainer = document.querySelector('.dropdownfillcontainer');
-const dropdownArrowDesign = document.querySelector('.js-arrow');
-const dropdownArrowFill = document.querySelector('.js-arrow-fill');
-
+const dropdownDesign = document.querySelector(".design");
+const dropdownFill = document.querySelector(".fill");
+const dropdownBtn = document.querySelector(".dropdown-btn");
+const dropdownFillBtn = document.querySelector(".dropdownfill-btn");
+const dropdownContainer = document.querySelector(".dropdowncontainer");
+const dropdownFillContainer = document.querySelector(".dropdownfillcontainer");
+const dropdownArrowDesign = document.querySelector(".js-arrow");
+const dropdownArrowFill = document.querySelector(".js-arrow-fill");
 
 function Dropdown() {
-    if (dropdownContainer.classList.contains('hidden')) {
-        dropdownContainer.classList.remove('hidden')
-        dropdownArrowDesign.classList.add('changeArrowDown')
-    } else {
-        dropdownContainer.classList.add('hidden')
-        dropdownArrowDesign.classList.remove('changeArrowDown')
-        dropdownArrowDesign.classList.add('changeArrowUp')
-    }
-};
-dropdownBtn.addEventListener('click', Dropdown);
+  if (dropdownContainer.classList.contains("hidden")) {
+    dropdownContainer.classList.remove("hidden");
+    dropdownFillContainer.classList.add("hidden");
+    dropdownArrowDesign.classList.add("changeArrowDown");
+  } else {
+    dropdownContainer.classList.add("hidden");
+    dropdownArrowDesign.classList.remove("changeArrowDown");
+    dropdownArrowDesign.classList.add("changeArrowUp");
+  }
+}
+dropdownBtn.addEventListener("click", Dropdown);
 
 function fillDropdown() {
-    if (dropdownFillContainer.classList.contains('hidden')) {
-        dropdownFillContainer.classList.remove('hidden')
-        dropdownArrowFill.classList.add('changeArrowDown')
-    } else {
-        dropdownFillContainer.classList.add('hidden')
-        dropdownArrowFill.classList.remove('changeArrowDown')
-        dropdownArrowFill.classList.add('changeArrowUp')
-    }
-};
-dropdownFillBtn.addEventListener('click', fillDropdown);
+  if (dropdownFillContainer.classList.contains("hidden")) {
+    dropdownFillContainer.classList.remove("hidden");
+    dropdownContainer.classList.add("hidden");
+    dropdownArrowFill.classList.add("changeArrowDown");
+  } else {
+    dropdownFillContainer.classList.add("hidden");
+    dropdownArrowFill.classList.remove("changeArrowDown");
+    dropdownArrowFill.classList.add("changeArrowUp");
+  }
+}
+dropdownFillBtn.addEventListener("click", fillDropdown);
 
 /*
 function() {
