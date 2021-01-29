cdff52fb4aaefb31e85c8d97ac42ca9cdd041125
lilymenal <liliana.mendoza.alvear@gmail.com>
2021-01-29 13:07:30 +0100

    fix jsdropdown changes

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-=======
 'use strict';
 
 const dropdownDesign = document.querySelector('.design');
@@ -15,17 +12,6 @@ const dropdownShare = document.querySelector ('.share');
 const dropdownShareBtn = document.querySelector ('.dropdownshare-btn');
 const dropdownShareContainer = document.querySelector('.dropdownsharecontainer');
 const dropdownArrowShare = document.querySelector('.js-arrow-share');
-
->>>>>>> pointer
-
-const dropdownDesign = document.querySelector(".design");
-const dropdownFill = document.querySelector(".fill");
-const dropdownBtn = document.querySelector(".dropdown-btn");
-const dropdownFillBtn = document.querySelector(".dropdownfill-btn");
-const dropdownContainer = document.querySelector(".dropdowncontainer");
-const dropdownFillContainer = document.querySelector(".dropdownfillcontainer");
-const dropdownArrowDesign = document.querySelector(".js-arrow");
-const dropdownArrowFill = document.querySelector(".js-arrow-fill");
 
 function Dropdown() {
   if (dropdownContainer.classList.contains("hidden")) {
