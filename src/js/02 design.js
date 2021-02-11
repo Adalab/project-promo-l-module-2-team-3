bd9628e2d719292beb650fe69c0fe5daeb43dea1
Marta Cano <martanchor@gmail.com>
2021-02-11 19:13:51 +0100

    icon colors

--- src/js/02 design.js
+++ src/js/02 design.js
@@ -4,19 +4,21 @@ const paletteElements = document.querySelectorAll(".js-palette");
 const cardElement = document.querySelector(".js-card");
 
 function updatePalette() {
-  // borro las clases palette-1, palette-2 y palette-3 del js-card
-  cardElement.classList.remove(
-    "palette-1",
-    "palette-2",
-    "palette-3",
-    "palette-4"
-  );
+    // borro las clases palette-1, palette-2 y palette-3 del js-card
+    cardElement.classList.remove(
+        "palette-1",
+        "palette-2",
+        "palette-3",
+        "palette-4"
+    );
 
-  // añado palette-x a js-card en función de la paleta seleccionada
-  const paletteValue = document.querySelector(".js-palette:checked").value;
-  cardElement.classList.add("palette-" + paletteValue);
-  saveInLocalStorage();
+    // añado palette-x a js-card en función de la paleta seleccionada
+    const mailFull = document.querySelector('.icon-mail');
+    const paletteValue = document.querySelector(".js-palette:checked").value;
+    cardElement.classList.add("palette-" + paletteValue);
+    mailFull.classList.add("fullicon-palette-" + paletteValue);
+    saveInLocalStorage();
 }
 for (const paletteElement of paletteElements) {
-  paletteElement.addEventListener("change", updatePalette);
+    paletteElement.addEventListener("change", updatePalette);
 }
